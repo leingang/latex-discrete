39acde0dc228617d6a0fb43ec602e01fa1207b35
Matthew Leingang <leingang@nyu.edu>
2019-05-29 23:39:54 -0400

    Add `update_tag` function to update source files

--- build.lua
+++ build.lua
@@ -4,6 +4,23 @@ module = "leindisc"
 
 unpackfiles = {"*.dtx"}
 
+
+function update_tag(file,content,tagname,tagdate)
+    tagdate_tex = string.gsub(tagdate,'-','/')
+    if string.match(file, "%.dtx$") then
+        content = string.gsub(content,
+            "\n\\ProvidesPackage{leindisc}%[%d%d%d%d/%d%d/%d%d v.- ",
+            "\n\\ProvidesPackage{leindisc}[" .. tagdate_tex .. " " .. tagname .. " "
+        )
+        content = string.gsub(content,
+            "\n%% \\changes{unreleased}",
+            "\n%% \\changes{" .. tagname .. "}"
+        )
+        return content
+    end
+end
+
+
 kpse.set_program_name("texlua")
 if not release_date then
    dofile(kpse.lookup("l3build.lua"))
